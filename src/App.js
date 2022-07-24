0d92fcda238b835f76f0796f536a5e38a1e0bbbb
Lurkbot9000 <guidry.brett@gmail.com>
2022-07-24 14:46:09 -0500

    PersonalProj initial install

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () =>{
+    return (
+        <h1>
+            Welcome to React App thats build using Webpack and Babel separately
+        </h1>
+    )
+}
+
+
+
+// changed this file name from App.js to App.jsx 
+// and added the "export default App" at the bottom
+export default App
